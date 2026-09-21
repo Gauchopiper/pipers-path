6353a08a935a4ea0bb591b622b23c6efbe260c13
Gauchopiper <137723488+Gauchopiper@users.noreply.github.com>
2026-09-21 14:32:57 -0700

    Add Beta TTT feedback config

---
+++ feedback-config.js
@@ -0,0 +1,3 @@
+// Set only after the separate Beta TTT feedback web app is deployed.
+// This public file must contain no credentials or private Google resource IDs.
+window.PIPERS_FEEDBACK = Object.freeze({ url: 'https://script.google.com/macros/s/AKfycby1uf9US_HCunVqklOwb_MmaIdN18mPW5yBmi7KO_pSujU4eBEy0nQ_jaGE6Dy8RM56yw/exec' });
