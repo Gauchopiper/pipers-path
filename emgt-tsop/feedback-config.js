62c4c0a7727caa04e949f39baf3ec0428fb409ac
Gauchopiper <137723488+Gauchopiper@users.noreply.github.com>
2026-09-24 07:46:41 -0700

    Add EMGT-TSOP hosted beta pupil page

---
+++ emgt-tsop/feedback-config.js
@@ -0,0 +1,2 @@
+// Set only to the separate EMGT-TSOP feedback web app /exec URL before publication.
+window.PIPERS_FEEDBACK = Object.freeze({ url: 'https://script.google.com/macros/s/AKfycbxu39UcigkpQnfsrJUKmObiEEBfqeM2-r-lerKbeYBpua_opyI51zkfCdtonsqueWp0/exec' });
